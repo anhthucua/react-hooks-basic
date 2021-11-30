58e5a9058957aa97a66f48ae0550aa0789144d55
Trần Thư <anhthu.3198@gmail.com>
2021-11-30 11:14:26 +0700

    05 - useState qua vidu TodoForm

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.scss';
 import ColorBox from './components/ColorBox';
 import React, { useState } from 'react';
 import TodoList from './components/TodoList';
+import TodoForm from './components/TodoForm';
 
 function App() {
   const [todoList, setTodoList] = useState([
@@ -19,11 +20,25 @@ function App() {
     setTodoList(newTodoList);
   }
 
+  function handleTodoFormSubmit(formValues) {
+    console.log('Form submit', formValues);
+
+    // add new todo to current todo list
+    const newTodo = {
+      id: todoList.length + 1,
+      ...formValues,
+    };
+
+    const newTodoList = [ ...todoList];
+    newTodoList.push(newTodo);
+    setTodoList(newTodoList);
+  }
+
   return (
     <div className="app">
       <h1>Welcome to react hooks!</h1>
 
-      <ColorBox />
+      <TodoForm onSubmit={handleTodoFormSubmit}/>
       <TodoList todos={todoList} onTodoClick={handleTodoClick}/>
     </div>
   );
