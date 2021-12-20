520cc7e0d419a8a2b3b8bb8a3d7b9c798fbd48b5
Anh Thu <anhthu.3198@gmail.com>
2021-12-20 14:08:00 +0700

    08 - pagination with effect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.scss';
 // import ColorBox from './components/ColorBox';
 import React, { useEffect, useState } from 'react';
+import queryString from 'query-string';
 import PostList from './components/PostList';
 import TodoList from './components/TodoList';
 import TodoForm from './components/TodoForm';
+import Pagination from './components/Pagination';
 
 function App() {
   const [todoList, setTodoList] = useState([
@@ -13,17 +15,30 @@ function App() {
   ])
 
   const [postList, setPostList] = useState([]);
+  const [pagination, setPagination] = useState({
+    _page: 1,
+    _limit: 10,
+    _totalRows: 11
+  })
+
+  const [filters, setFilters] = useState({
+    _limit: 10,
+    _page: 1,
+  })
 
   useEffect(() => {
     async function fetchPostList() {
       try { 
-        const requestUrl = 'https://js-post-api.herokuapp.com/api/posts?_limit=10&+page=1';
+        //- _limit=10&_page=1
+        const paramsString = queryString.stringify(filters); // bien tu object sang chuoi
+        const requestUrl = `https://js-post-api.herokuapp.com/api/posts?${paramsString}`;
         const response = await fetch(requestUrl);
         const responseJSON = await response.json();
         console.log(responseJSON);
 
-        const {data} = responseJSON;
+        const {data, pagination} = responseJSON;
         setPostList(data);
+        setPagination(pagination);
       } catch (error) {
         console.log('Fail to fetch post list: ', error.message);
       }
@@ -31,11 +46,19 @@ function App() {
 
     console.log('post list effect');
     fetchPostList();
-  }, [])
+  }, [filters])
 
   useEffect(() => {
     console.log('todo list effect');
   });
+
+  function handlePageChange(newPage) {
+    console.log('Mew page ', newPage);
+    setFilters({
+      ...filters,
+      _page: newPage,
+    })
+  }
 
   function handleTodoClick(todo) {
     const index = todoList.findIndex(x => x.id === todo.id);
@@ -68,6 +91,10 @@ function App() {
       <TodoList todos={todoList} onTodoClick={handleTodoClick}/>
 
       <PostList posts={postList}/>
+      <Pagination
+        pagination={pagination}
+        onPageChange={handlePageChange}
+      />
     </div>
   );
 }
